6ba944ca675eed7dd41e0fa55b0ba666defacb52
David Alberto Adler <dalberto.adler@gmail.com>
2023-12-07 14:40:35 +0000

    Fix isProduction check

--- deno/src/utils/isProduction.ts
+++ deno/src/utils/isProduction.ts
@@ -1,2 +1,2 @@
 // there is probably a better way of working out if in production mode
-export const isProduction = Boolean(Deno.env.get('github_client_id'))
+export const isProduction = Boolean(Deno.env.get('github_token'))
